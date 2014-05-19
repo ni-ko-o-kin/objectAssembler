fb12c5a621eda88cd54c2ebb8d233f95f4066770
Nikolaus Morawitz <niko@emptygraphics.com>
2014-05-19 18:19:34 +0200

    added menu-improvements from draw_model_icon-branch

--- mode/menu.py
+++ mode/menu.py
@@ -3,6 +3,14 @@ from ..common.common import get_tool_shelf_width
 
 DEBUG = False
 
+
+class MC(): # MenuConstants
+    PAD = 2           # distance between icons and between icons and border
+    PAD_CATEGORIES = 20   # distance between categories
+    PAD_TOP = 65      # distance between region-height-top and frame
+    PAD_LEFT = 22     # distance between region-width-left and frame
+    HOVER = 0         # border around icons for hover-effect
+
 def construct_menu(settings):
     simps_impls = settings.models.simps_impls
 
@@ -24,27 +32,21 @@ def construct_menu(settings):
     ##########################
     ### generate geometry ###
     # oa_icons.png has to be square because of UV-precision
-    
-    ICON_DISPLAY = settings.menu_icon_display_size # display size
-    COL_MAX = settings.menu_columns                # maximum number of columns
-    ICON_PIXEL = settings.menu_icon_size           # height and width of the icons in oa_icons.png
-    PAD = 2           # distance between icons and between icons and border
-    PAD_GROUPS = 20   # distance between groups
-    PAD_TOP = 65      # distance between region-height-top and frame
-    PAD_LEFT = 22     # distance between region-width-left and frame
-    HOVER = 0         # border around icons for hover-effect
+    icon_display = settings.menu_icon_display_size # display size
+    col_max = settings.menu_columns                # maximum number of columns
+    icon_pixel = settings.menu_icon_size           # height and width of the icons in oa_icons.png    
 
     img = bpy.data.images["oa_icons.png", settings.oa_file]
-    subdivisions = img.size[0] / ICON_PIXEL
+    subdivisions = img.size[0] / icon_pixel
 
     # get the tool shelf width
     tool_shelf_width = get_tool_shelf_width(bpy.context)
             
-    # calculate all x-positions: at COL_MAX==3 these would be 3 values
-    pos_x = [tool_shelf_width + PAD_LEFT + i * (ICON_DISPLAY + PAD) for i in range(COL_MAX)]
+    # calculate all x-positions: at col_max==3 these would be 3 values
+    pos_x = [tool_shelf_width + MC.PAD_LEFT + i * (icon_display + MC.PAD) for i in range(col_max)]
 
     # calculate startvalues for y-positions
-    pos_y = bpy.context.region.height - PAD_TOP - ICON_DISPLAY
+    pos_y = bpy.context.region.height - MC.PAD_TOP - icon_display
     
     # list of all icon-relevant information
     # [
@@ -68,7 +70,7 @@ def construct_menu(settings):
         print("\nMenu:")
         oa_ids_for_debug = ""
 
-    # iterate over groups
+    # iterate over categories
     for groupings in structured:
         # iterate over obj
         col = 0
@@ -79,31 +81,35 @@ def construct_menu(settings):
                     oa_ids_for_debug += "{" + str(oa_id[2])
                 else:
                     oa_ids_for_debug = ', '.join((oa_ids_for_debug, str(oa_id[2])))
-            if col >= COL_MAX:
-                pos_y -= (ICON_DISPLAY + PAD)
+            if col >= col_max:
+                pos_y -= (icon_display + MC.PAD)
                 col = 0
 
-            icon = (pos_x[col], pos_y, pos_x[col] + ICON_DISPLAY, pos_y + ICON_DISPLAY)
+            icon = (pos_x[col], pos_y, pos_x[col] + icon_display, pos_y + icon_display)
+            frame = (icon[0] - MC.PAD, icon[1] - MC.PAD, icon[2] + \
+                         MC.PAD, icon[3] + MC.PAD)
+            hover = (icon[0] - MC.HOVER, icon[1] - MC.HOVER, icon[2] + MC.HOVER, icon[3] + MC.HOVER)
+            uv = (
+                (oa_id[2] * 1/subdivisions, oa_id[1] * 1/subdivisions),
+                (oa_id[2] * 1/subdivisions, (oa_id[1] + 1) * 1/subdivisions),
+                ((oa_id[2] + 1) * 1/subdivisions, (oa_id[1] + 1) * 1/subdivisions),
+                ((oa_id[2] + 1) * 1/subdivisions, oa_id[1] * 1/subdivisions)
+                )
             
             icons.append(
                 (
-                    oa_id, # id
-                    icon, # icon
-                    (icon[0] - PAD, icon[1] - PAD, icon[2] + PAD, icon[3] + PAD), # frame
-                    (icon[0] - HOVER, icon[1] - HOVER, icon[2] + HOVER, icon[3] + HOVER), # hover
-                    (
-                        (oa_id[2] * 1/subdivisions, oa_id[1] * 1/subdivisions),
-                        (oa_id[2] * 1/subdivisions, (oa_id[1] + 1) * 1/subdivisions),
-                        ((oa_id[2] + 1) * 1/subdivisions, (oa_id[1] + 1) * 1/subdivisions),
-                        ((oa_id[2] + 1) * 1/subdivisions, oa_id[1] * 1/subdivisions)
-                        )  # uv
+                    oa_id,
+                    icon,
+                    frame,
+                    hover,
+                    uv
                     )
                 )
 
             col += 1
             
-        # after every group add a PAD_GROUPS + ICON_DISPLAY
-        pos_y -= (PAD_GROUPS + ICON_DISPLAY)
+        # after every group add a MC.PAD_CATEGORIES + icon_display
+        pos_y -= (MC.PAD_CATEGORIES + icon_display)
                         
         if DEBUG: print("oa_ids: " + oa_ids_for_debug + "}")
 
