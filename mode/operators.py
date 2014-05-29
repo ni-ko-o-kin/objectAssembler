acaffa87501fb96f530de787bcd3587182736b05
Nikolaus Morawitz <niko@emptygraphics.com>
2014-05-29 20:41:55 +0200

    gl_touch should fix the 'textures turn black after a while-bug'

--- mode/operators.py
+++ mode/operators.py
@@ -184,10 +184,13 @@ class OAEnterOAMode(bpy.types.Operator):
         return
 
     def modal(self, context, event):
+        settings = bpy.context.scene.OASettings
         if context.area:
             context.area.tag_redraw()
+        if settings.valid_icon_file:
+            self.img.gl_touch()
+
         self.ctrl = event.ctrl
-        settings = bpy.context.scene.OASettings
         if not settings.oa_mode_started and self._handle is not None:
             bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
             if settings.valid_icon_file:
